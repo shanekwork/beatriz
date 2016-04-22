c124fefdd1a09991fb88aeff253f82ca956811d4
beaic3 <beaic3ss@hotmail.com>
2016-04-22 13:08:49 +0100

    Add icon to favourite link on products page

--- app/overrides/add_link_to_mark_product_as_favorite.rb
+++ app/overrides/add_link_to_mark_product_as_favorite.rb
@@ -4,9 +4,15 @@ Deface::Override.new(
   :insert_after => "div[itemprop='after-fav']",
   :text => %Q{
     <% if spree_user_signed_in? && spree_current_user.has_favorite_product?(@product.id) %>
-      <%= link_to Spree.t(:unmark_as_favorite), favorite_product_path(:id => @product.id), :method => :delete, :remote => true, :class => 'favorite_link' %>
+      <div class="favourite-product">
+        <span class="glyphicons glyphicons-heart-empty" aria-hidden="true"></span>
+        <%= link_to Spree.t(:unmark_as_favorite), favorite_product_path(:id => @product.id), :method => :delete, :remote => true, :class => 'favorite_link' %>
+      </div>
     <% else %>
-      <%= link_to Spree.t(:mark_as_favorite), favorite_products_path(:id => @product.id), :method => :post, :remote => spree_user_signed_in?, :class => 'favorite_link' %>
+      <div class="favourite-product">
+        <span class="glyphicons glyphicons-heart" aria-hidden="true"></span>
+        <%= link_to Spree.t(:mark_as_favorite), favorite_products_path(:id => @product.id), :method => :post, :remote => spree_user_signed_in?, :class => 'favorite_link' %>
+      </div>
     <% end %>
   }
 )
